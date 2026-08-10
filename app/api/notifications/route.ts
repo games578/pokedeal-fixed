0f72571d23ba16a4566a775e5bc671ab0c9de8eb
games578 <vladislavpilipchuk11@gmail.com>
2026-08-10 16:00:07 +0100

    Delete app directory

--- app/api/notifications/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { listUnseenNotifications } from "@/lib/repo/notifications";
-
-export async function GET() {
-  return NextResponse.json({ notifications: await listUnseenNotifications() });
-}
