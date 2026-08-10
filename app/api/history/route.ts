8400642ed4b78797926c85ed605be3974a5a6e1f
games578 <vladislavpilipchuk11@gmail.com>
2026-08-11 00:25:52 +0100

    Delete app directory

--- app/api/history/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { listDeals } from "@/lib/repo/deals";
-
-export async function GET() {
-  const deals = await listDeals({});
-  return NextResponse.json({ deals });
-}
